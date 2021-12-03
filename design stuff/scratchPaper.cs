01aa33d90f37a4f385fcd2f56ce247d3ca4aad53
Herbert Smith <dartomictech@gmail.com>
2021-12-02 20:02:20 -0500

    Make design folder
    Decided the scratch paper, and maybe feature ideas if I make it a file, could be in the repository. This way if I lose all my work on the PC, they will be here.

---
+++ design stuff/scratchPaper.cs
@@ -0,0 +1,72 @@
+/****************************************************************
+                        scratchPaper.cs
+This file is normally used to design components before
+putting them into the program.
+****************************************************************/
+
+
+        /****************MODIFIED VERSION******************************/
+
+
+        private static void CollectStudyX()
+        {
+
+            DateTime useDate, nextDate;
+            int dateCompare;
+            useDate = DateTime.Parse(predictVars.Sim_Date_Use);
+
+            // Console.WriteLine("Inside CollectStudyX");
+            // Console.WriteLine($"DELETEME studiedSimList.Count = {studiedSimList.Count}");
+            // Console.ReadLine();
+
+
+            // Get 2nd rep studies
+            int index, repCheck;
+            index = repCheck = Constants.ZERO_INT;
+            foreach (var topic in genSimsAll)
+            {
+                nextDate = DateTime.Parse(topic.Next_Date);
+                dateCompare = DateTime.Compare(nextDate, useDate);
+
+                if (dateCompare <= Constants.ZERO_INT && topic.Real_Repetition == Constants.TWO_INT)
+                    if (repCheck < predictVars.X_High_Xcount)
+                    {
+                        studyRepElements.Add(index);
+                        ++repCheck;
+                    }
+                ++index;
+            }
+
+            // Get Late
+            index = Constants.ZERO_INT;
+            foreach (var topic in genSimsAll)
+            {
+                nextDate = DateTime.Parse(topic.Next_Date);
+                dateCompare = DateTime.Compare(nextDate, useDate);
+
+                if (dateCompare < Constants.ZERO_INT && topic.Real_Repetition != Constants.TWO_INT)
+                    if (repCheck < predictVars.X_High_Xcount)
+                    {
+                        studyRepElements.Add(index);
+                        ++repCheck;
+                    }
+                ++index;
+            }
+
+            //Get On-Time
+            index = Constants.ZERO_INT;
+            foreach (var topic in genSimsAll)
+            {
+                nextDate = DateTime.Parse(topic.Next_Date);
+                dateCompare = DateTime.Compare(nextDate, useDate);
+
+                if (dateCompare == Constants.ZERO_INT && topic.Real_Repetition != Constants.TWO_INT)
+                    if (repCheck < predictVars.X_High_Xcount)
+                    {
+                        studyRepElements.Add(index);
+                        ++repCheck;
+                    }
+                ++index;
+            }
+            predictVars.Current_X = repCheck;
+        }
